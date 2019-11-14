d441b02cf4a206269bf5a181fe77e9db8506221f
iamdashjei <jovitopangan123@gmail.com>
2019-11-14 21:56:04 +0800

    Changes again blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Lacto-B Incorporated</title>
+        <title>CarToGo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
